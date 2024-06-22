48b8ebe6d5cf387b34b6d706d74598ebe8a758d2
tamir.rustamov <timorust1@gmail.com>
2024-06-22 06:21:43 +0300

    upload image

--- components/collections/CollectionForm.tsx
+++ components/collections/CollectionForm.tsx
@@ -4,6 +4,8 @@ import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
+
 import {
 	Form,
 	FormControl,
@@ -14,6 +16,7 @@ import {
 	FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import ImageUpload from '../custom ui/ImageUpload'
 
 const formSchema = z.object({
 	title: z.string().min(2).max(50),
@@ -37,7 +40,7 @@ function CollectionForm() {
 
 	return (
 		<div className='p-10'>
-			<p className='text-heading2-bold'>Collection-Form</p>
+			<p className='text-heading2-bold'>Collection Form</p>
 			<Separator className='bg-grey-1 mt-4 mb-7' />
 
 			<Form {...form}>
@@ -47,18 +50,50 @@ function CollectionForm() {
 						name='title'
 						render={({ field }) => (
 							<FormItem>
-								<FormLabel>Username</FormLabel>
+								<FormLabel>Title</FormLabel>
 								<FormControl>
-									<Input placeholder='shadcn' {...field} />
+									<Input placeholder='Title' {...field} />
 								</FormControl>
-								<FormDescription>
-									This is your public display name.
-								</FormDescription>
+
 								<FormMessage />
 							</FormItem>
 						)}
 					/>
-					<Button type='submit'>Submit</Button>
+					<FormField
+						control={form.control}
+						name='description'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Description</FormLabel>
+								<FormControl>
+									<Textarea placeholder='Description' {...field} rows={5} />
+								</FormControl>
+
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='image'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Image</FormLabel>
+								<FormControl>
+									<ImageUpload
+										value={field.value ? [field.value] : []}
+										onChange={url => field.onChange(url)}
+										onRemove={() => field.onChange('')}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<div>
+						<Button type='submit'>Submit</Button>
+						<Button type='submit'>Submit</Button>
+					</div>
 				</form>
 			</Form>
 		</div>
