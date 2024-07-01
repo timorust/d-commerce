b0017989f64a944905e4825204b7122ef56f8b81
tamir.rustamov <timorust1@gmail.com>
2024-07-01 16:05:26 +0300

    multi select

--- components/custom ui/MultiSelect.tsx
+++ components/custom ui/MultiSelect.tsx
@@ -35,30 +35,55 @@ const MultiSelect: React.FC<MultiSelectProps> = ({
 
 	let selected: CollectionType[]
 
-	if (value.length === 0) selected = []
-	else {
+	if (value.length === 0) {
+		selected = []
+	} else {
 		selected = value.map(id =>
 			collections.find(collection => collection._id === id)
 		) as CollectionType[]
 	}
 
+	const selectables = collections.filter(
+		collection => !selected.includes(collection)
+	)
+
 	return (
 		<Command className='overflow-visible bg-white'>
-			<CommandInput
-				placeholder={placeholder}
-				value={inputValue}
-				onValueChange={setInputValue}
-				onBlur={() => setOpen(false)}
-				onFocus={() => setOpen(true)}
-			/>
+			<div className='flex gap-1 flex-wrap border rounded-md'>
+				{selected.map(collection => (
+					<Badge key={collection._id}>
+						{collection.title}
+						<button
+							type='button'
+							className='ml-1 hover:text-red-2'
+							onClick={() => onRemove(collection._id)}
+						>
+							<X className='h-3 w-3' />
+						</button>
+					</Badge>
+				))}
+
+				<CommandInput
+					placeholder={placeholder}
+					value={inputValue}
+					onValueChange={setInputValue}
+					onBlur={() => setOpen(false)}
+					onFocus={() => setOpen(true)}
+				/>
+			</div>
+
 			<div className='relative mt-2'>
 				{open && (
 					<CommandGroup className='absolute w-full z-30 top-0 overflow-auto border rounded-md shadow-md'>
-						{collections.map(collection => (
+						{selectables.map(collection => (
 							<CommandItem
 								key={collection._id}
 								onMouseDown={e => e.preventDefault()}
-								onSelect={() => onChange(collection._id)}
+								onSelect={() => {
+									onChange(collection._id)
+									setInputValue('')
+								}}
+								className='hover:bg-grey-3 cursor-pointer'
 							>
 								{collection.title}
 							</CommandItem>
